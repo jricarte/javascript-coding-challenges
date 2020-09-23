ea1bb365361509417d1a77701fac6f3ac8ce4d3b
Jordan Ricarte <jordan@ricarte.net>
2020-09-23 10:02:13 -0500

    Loop example practice

--- test.js
+++ test.js
@@ -22,5 +22,19 @@ console.log(typeOf);
 
 
 
+var a = "42";
 
+var b = Number( a );
+
+console.log(a);
+console.log(b);
+
+// 3. FizzBuzz Challenge
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+
+for (let i = 1; i <= 100; i++) {
+    let f = i % 3 == 0,
+    b = i % 5 == 0;
+    console.log(f ? (b ? 'FizzBuzz' : 'Fizz') : b ? 'Buzz' : i);
+}
 
