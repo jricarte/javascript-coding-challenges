5b9a08b9fc101333a47d3b6a1d31ce66c810a9fd
Jordan Ricarte <jordan@ricarte.net>
2020-09-22 22:34:39 -0500

    first couple practice questions & answers

---
+++ test.js
@@ -0,0 +1,26 @@
+// 1. What is Coercion in JavaScript?
+// ANSWER: Coercion is the coversion between two different build-in types
+
+// Explicit Coercion
+var a = "42";
+var b = Number(a);
+
+console.log(a); 
+// "42"
+console.log(b);
+// 42 number type
+
+// 2. What is typeof operator?
+// ANSWER: typeof is a operator that can examine a value and tell you what type it is:
+
+var typeOf = "i am a string"
+typeof typeOf;
+
+console.log(typeOf);
+
+// Do this example in google chrome console
+
+
+
+
+
